3a4d6c9d8c664a7aafcca3dbf52c046990fcdbf4
Dournbrood <tanner1220002@gmail.com>
2019-11-18 18:31:49 -0500

    added active button state to nav

--- saltiest-hacker-news-trolls-1/src/components/Navigation.js
+++ saltiest-hacker-news-trolls-1/src/components/Navigation.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Nav, NavItem, NavLink, } from 'reactstrap';
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+
+    const [activeButton, setActiveButton] = useState();
+
     return (
         <Nav pills className="p-2 d-flex justify-content-around mb-2 shadow-sm">
             <NavItem>
-                <NavLink> <Link to="/login">Login</Link></NavLink>
+                <Link to="/login"><NavLink active={activeButton === 1} onClick={() => setActiveButton(1)}>Login</NavLink></Link>
             </NavItem>
             <NavItem>
-                <NavLink><Link to="/signup">Sign Up</Link></NavLink>
+                <Link to="/signup"><NavLink active={activeButton === 2} onClick={() => setActiveButton(2)}>Sign Up</NavLink></Link>
             </NavItem>
         </Nav>
     );
