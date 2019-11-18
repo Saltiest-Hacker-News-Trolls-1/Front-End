826a72bb26e6df5f6466576891829d255bf8281d
Dournbrood <tanner1220002@gmail.com>
2019-11-18 13:34:57 -0500

    added missing imports, more structure

---
+++ saltiest-hacker-news-trolls-1/src/components/LoginPage.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+
+const LoginForm = () => {
+
+}
+
+const FormikLoginForm = withFormik({
+
+})(LoginForm)
+
+export default FormikLoginForm
